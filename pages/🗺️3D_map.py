8eb3fcb85d354aed5b49c84134ad1da935ce9a1f
liuchia515 <tinaliu0515@gmail.com>
2024-11-29 00:29:31 +0800

    Update 🗺️3D_map.py

--- pages/🗺️3D_map.py
+++ pages/🗺️3D_map.py
@@ -9,20 +9,5 @@ st.set_page_config(layout="wide", page_title="taiwan earthquake 3d map")
 path="https://github.com/liuchia515/gisapp_hw10/blob/main/difference_result.geojson"
 
 m = leafmap.Map(center=[23.5, 121], zoom=7)
-m.add_geojson(
-    path,
-    layer_type="fill-extrusion",
-    name="difference",
-    paint={
-        "fill-extrusion-color": ["get", "color"],
-        "fill-extrusion-height": ["*", ["get", "difference"], 50],
-        "fill-extrusion-opacity": 0.8,
-    },
-)
-m.add_colorbar(
-    cmap="coolwarm",
-    label="Difference",
-    orientation="horizontal",
-
-)
+m.add_geojson(path,layer_name="difference")
 m.to_streamlit(height=500)
