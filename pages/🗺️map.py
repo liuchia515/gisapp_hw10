9688328b8365893477f2af85ca6af1f544572a4b
liuchia515 <tinaliu0515@gmail.com>
2024-11-29 04:03:16 +0800

    Update 🗺️map.py

--- pages/🗺️map.py
+++ pages/🗺️map.py
@@ -6,11 +6,11 @@ import numpy as np
 import pandas as pd
 import pydeck as pdk
 
-st.title("map")
+st.title("map a")
 
 with st.expander("See source code"):
   with st.echo():
     m=leafmap.Map(center=[23.5, 121], zoom=7)
     path="https://raw.githubusercontent.com/liuchia515/gisapp_hw10/main/difference_result.geojson"
-    m.add_geojson(path,name="valuePerSqm")
+    m.add_geojson(path,name="difference")
 m.to_streamlit(height=500)
