5d316b0b3f946a08635d5cd2f354b6d9e103c93e
liuchia515 <tinaliu0515@gmail.com>
2024-11-29 00:44:45 +0800

    Update 🗺️3D_map.py

--- pages/🗺️3D_map.py
+++ pages/🗺️3D_map.py
@@ -6,7 +6,7 @@ st.title("taiwan earthquake 3d map")
 
 with st.expander("See source code"):
   with st.echo():
-    m=leafmap.Map(center[23.5,121],zoom=7)
+    m=leafmap.Map(center=[23.5,121],zoom=7)
     path="https://github.com/liuchia515/gisapp_hw10/blob/main/difference_result.geojson"
     m.add_geojson(data,layer_name="difference",add_legend=True)
 m.to_streamlit(height=500)
