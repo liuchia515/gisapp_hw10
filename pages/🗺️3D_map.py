dbe7393ec3ac98b9198114a6021d2825c436b2d3
liuchia515 <tinaliu0515@gmail.com>
2024-11-29 00:45:08 +0800

    Update 🗺️3D_map.py

--- pages/🗺️3D_map.py
+++ pages/🗺️3D_map.py
@@ -8,5 +8,5 @@ with st.expander("See source code"):
   with st.echo():
     m=leafmap.Map(center=[23.5,121],zoom=7)
     path="https://github.com/liuchia515/gisapp_hw10/blob/main/difference_result.geojson"
-    m.add_geojson(data,layer_name="difference",add_legend=True)
+    m.add_geojson(data,layer_name="difference",add_legend=True,)
 m.to_streamlit(height=500)
