9e613db69605b96d1e34887015952de9e25e2d1c
liuchia515 <tinaliu0515@gmail.com>
2024-11-29 03:16:51 +0800

    Update 🗺️map.py

--- pages/🗺️map.py
+++ pages/🗺️map.py
@@ -8,6 +8,6 @@ st.title("taiwan earthquake map")
 with st.expander("See source code"):
   with st.echo():
     m=leafmap.Map(center=[23.5,121],zoom=7)
-    path="https://github.com/liuchia515/gisapp_hw10/blob/main/difference_result.geojson"
+    path="difference_result.geojson"
     m.add_geojson(path,layer_name="difference")
 m.to_streamlit(height=500)
