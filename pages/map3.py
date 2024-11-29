56b43ad8b4f60acec8af75badadb1e69e018b38a
liuchia515 <tinaliu0515@gmail.com>
2024-11-29 21:33:09 +0800

    Update map3.py

--- pages/map3.py
+++ pages/map3.py
@@ -1,17 +1,40 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
+import pandas as pd
+import numpy as np
+import pydeck as pdk
 
-st.set_page_config(layout="wide")
-st.title("🗺️map3")
+chart_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
 
-with st.expander("See source code"):
-    with st.echo():
-        filepath = "https://raw.githubusercontent.com/liuchia515/gisapp_hw10/main/difference_result.geojson"
-        m = leafmap.Map(center=[23.5, 121], zoom=7)
-        m.add_heatmap(
-            filepath,
-            value="difference",
-            name="Heat map",
-            radius=20,
-        )
-m.to_streamlit(height=700)
+st.pydeck_chart(
+    pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=37.76,
+            longitude=-122.4,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=chart_data,
+                get_position="[lon, lat]",
+                radius=200,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            ),
+            pdk.Layer(
+                "ScatterplotLayer",
+                data=chart_data,
+                get_position="[lon, lat]",
+                get_color="[200, 30, 0, 160]",
+                get_radius=200,
+            ),
+        ],
+    )
+)
