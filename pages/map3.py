5778b038b5c475c5a3e7bd3fca28ca5e14cf2eaa
liuchia515 <tinaliu0515@gmail.com>
2024-11-29 22:50:34 +0800

    Update map3.py

--- pages/map3.py
+++ pages/map3.py
@@ -1,3 +1,5 @@
+import os
+import altair as alt
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -5,32 +7,37 @@ import pydeck as pdk
 
 st.set_page_config(layout="wide")
 st.title("🗺️map3")
+@st.cache_resource
+def load_data():
+  path = "https://raw.githubusercontent.com/liuchia515/gisapp_hw10/refs/heads/main/GDMScatalog.csv"
+  data = pd.read_csv(
+    path,
+    names=["date","time","lat","lon","depth","ML",], 
+    skiprows=1,
+  )
+  return data
 
-path = "https://raw.githubusercontent.com/liuchia515/gisapp_hw10/refs/heads/main/GDMScatalog.csv"
-data = pd.read_csv(
-  path,
-  names=["date","time","lat","lon","depth","ML",], 
-)
-st.pydeck_chart(
-    pdk.Deck(
-        map_style=None,
-        initial_view_state=pdk.ViewState(
-            latitude=23.5,
-            longitude=121,
-            zoom=7,
-            pitch=50,
-        ),
-        layers=[
-            pdk.Layer(
-                "HexagonLayer",
-                data=data,
-                get_position="[lon, lat]",
-                radius=200,
-                elevation_scale=4,
-                elevation_range=[0, 1000],
-                pickable=True,
-                extruded=True,
-            ),
-        ],
+def map(data, lat, lon, zoom):
+    st.write(
+        pdk.Deck(
+            map_style="mapbox://styles/mapbox/light-v9",
+            initial_view_state={
+                "latitude": lat,
+                "longitude": lon,
+                "zoom": zoom,
+                "pitch": 50,
+            },
+            layers=[
+                pdk.Layer(
+                    "HexagonLayer",
+                    data=data,
+                    get_position=["lon", "lat"],
+                    radius=100,
+                    elevation_scale=4,
+                    elevation_range=[0, 10],
+                    pickable=True,
+                    extruded=True,
+                ),
+            ],
+        )
     )
-)
