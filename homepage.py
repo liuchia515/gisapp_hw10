3071776e073fd907bae792b7afe515737038e871
liuchia515 <tinaliu0515@gmail.com>
2024-11-29 21:49:13 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -15,4 +15,9 @@ m=leafmap.Map(minimap_control=True,center=[23.5, 121], zoom=7)
 m.add_basemap("OpenTopoMap")
 m.to_streamlit(height=500)
 
+st.markdown(
+  """
+  1973年後，規模5以上的地震分布
+  """
+)
 st.image("ML_map.png", caption="ML_map")
