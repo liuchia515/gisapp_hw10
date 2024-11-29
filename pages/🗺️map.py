fec032b745061f71309d33f1869a75ac0c31ca13
liuchia515 <tinaliu0515@gmail.com>
2024-11-29 18:22:41 +0800

    Update 🗺️map.py

--- pages/🗺️map.py
+++ pages/🗺️map.py
@@ -7,11 +7,13 @@ import pandas as pd
 import pydeck as pdk
 
 st.set_page_config(layout="wide")
-st.title("map a")
+st.title("🗺️map")
+markdown1="觀測值與預估值之差異視覺化"
+st.markdown(markdown1)
 
 with st.expander("See source code"):
   with st.echo():
-    m = leafmap.Map(center=[23.5, 121], zoom=7)
+    m = leafmap.Map(center=[23.5, 121], zoom=7,minimap_control=True)
     path = "https://raw.githubusercontent.com/liuchia515/gisapp_hw10/main/difference_result.geojson"
     m.add_geojson(path,layer_name="difference")
 m.to_streamlit(height=700)
