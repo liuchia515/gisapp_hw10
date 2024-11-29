e0c2b2b7d238fc032e7c2b140f678a0f0bd2beb0
liuchia515 <tinaliu0515@gmail.com>
2024-11-29 23:40:56 +0800

    Update map3.py

--- pages/map3.py
+++ pages/map3.py
@@ -11,23 +11,26 @@ def load_data(nrows):
     return df
 df=load_data(1000)
 
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state={
-        "latitude":"lat",
-        "lontitude":"lon",
-        "zoom":7,
-        "pitch":50,
-    },
-    layers=[
-        pdk.Layer(
-            "HexagonLayer",
-            data=df[["ML","lat","lon"]],
-            get_position=["lat","lon"],
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0,10],
-            pickable=True,
-            extruded=True,
-        ),
-    ]))
+st.pydeck_chart(
+    pdk.Deck(
+        map_style=None,
+        initial_view_state={
+            "latitude":"lat",
+            "lontitude":"lon",
+            "zoom":7,
+            "pitch":50,
+        },
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=df[["ML","lat","lon"]],
+                get_position=["lat","lon"],
+                radius=200,
+                elevation_scale=4,
+                elevation_range=[0,10],
+                pickable=True,
+                extruded=True,
+            ),
+        ]
+    )
+)
