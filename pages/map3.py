01546c747e053a20b96c254c7e360283bb907a0d
liuchia515 <tinaliu0515@gmail.com>
2024-11-29 20:45:39 +0800

    Update map3.py

--- pages/map3.py
+++ pages/map3.py
@@ -6,35 +6,33 @@ import pydeck as pdk
 st.set_page_config(layout="wide")
 st.title("🗺️map3")
 url = "https://raw.githubusercontent.com/liuchia515/gisapp_hw10/main/difference_result.geojson"
+data=gpd.read_file(url)
 
-chart_data = pd.DataFrame(url)
-st.pydeck_chart(
-    pdk.Deck(
-        map_style=None,
-        initial_view_state=pdk.ViewState(
-            latitude=23.5,
-            longitude=121,
-            zoom=7,
-            pitch=50,
-        ),
-        layers=[
-            pdk.Layer(
-                "HexagonLayer",
-                data=chart_data,
-                get_position="[lon, lat]",
-                radius=200,
-                elevation_scale=4,
-                elevation_range=[0, 1000],
-                pickable=True,
-                extruded=True,
-            ),
-            pdk.Layer(
-                "ScatterplotLayer",
-                data=chart_data,
-                get_position="[lon, lat]",
-                get_color="[200, 30, 0, 160]",
-                get_radius=200,
-            ),
-        ],
+def map_3d(data,zoom):
+    geojson = data.to_json
+    st.write(
+        pdk.Deck(
+            map_style=None,
+            initial_view_stste={
+                "latitude":data.geometry.centroid.y.mean(),
+                "longitude": data.geometry.centroid.x.mean(),
+                "zoom": zoom,
+                "pitch": 50,
+            },
+            layers=[
+                pdk.Layer(
+                    "GeoJsonLayer",
+                    data=geojson_data,
+                    pickable=True,
+                    stroked=False,
+                    filled=True,
+                    extruded=True,
+                    get_fill_color=["get", "color"],
+                    get_elevation="difference",
+                    opacity=0.8,
+                ),
+            ],
+        )
     )
-)
+st.title("3D GeoJSON 地圖展示")
+map_3d(data, zoom=7)
